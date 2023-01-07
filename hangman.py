b0b4618b59aec52bb200c89bd552cdfdf8cd4ec0
Marcos <marcosamorandi@gmail.com>
2023-01-07 15:59:19 -0300

    Projeto quase finalizado.

--- hangman.py
+++ hangman.py
@@ -1,12 +1,12 @@
 import random # Importação do módulo random (aleatório).
-from palavras import words # Do arquivo "palavras.py" importa a variável "words".
-from visual import lives_visual # Do arquivo "visual.py" importa a variável "lives_visual" responsável pelo "gráfico".
-import string # Importação do módulo string, não é consenso sua utilidade na versão atual do Python, mas por via das dúvidas não custa colocar.
+from palavras import words # Do arquivo "palavras.py", importa a variável "words".
+from visual import lives_visual # Do arquivo "visual.py", importa a variável "lives_visual", responsável pelo "gráfico".
+import string # Importação do módulo string, não é consenso sua utilidade na versão atual do Python, mas para evitar problemas não custa importar.
 
-# Depois que tirei manualemnte hífens, numeros e espaços em branco das palavras a função abaixo, ficou obsoleta. Teria que mexer em várias partes do código para arrumar tudo.
+# Depois que tirei manualmente hifens, numeros e espaços em branco das palavras, a função abaixo ficou obsoleta. Não quis mexer em várias partes do código pelo prazo de entrega.
 def get_valid_word(words): # "def" cria a função "get_valid_word" com a variável "words", ou seja, palavra válida, que no caso é sem hífen ou espaço.
     word = random.choice(words)  # Aleatóriamente escolhe uma palavra da lista.
-    while '-' in word or ' ' in word: # Enquanto for escolhida palavra com hífens ou espaço...
+    while '-' in word or ' ' in word: # Enquanto for escolhida palavra com hifens ou espaço...
         word = random.choice(words) # Repete o comando de escolha de palavra aleatória.
 
     return word.upper() # Retorna uma palavra e o ".upper()" converte em maiúscula.
@@ -24,10 +24,10 @@ print('============================================================')
 
 
 def hangman(): # Cria a função do jogo.
-    word = get_valid_word(words) # Recebe uma palavra válida.
-    word_letters = set(word)  # Define letras na palavra.
-    alphabet = set(string.ascii_uppercase) # Fornece as letras de A a Z em maiúsculas (ASCII é americano e diferente de ABNT com Ç etc).
-    used_letters = set()  # O que o usuário já advinhou.
+    word = get_valid_word(words) # Variável que recebe uma palavra válida da variável importada "words".
+    word_letters = set(word)  # "set()" é usado para armazenar múltiplos itens em uma única variável. Criada a variável "word_letters" ou letras_na_palavra. 
+    alphabet = set(string.ascii_uppercase) # Declarada variável "alphabet", que vai receber múltiplos strings ASCII e transformar em maiúsculas, "string.ascii_uppercase".
+    used_letters = set()  # Variável que armazena letras já usadas.
 
     lives = 7 # Define a quantidade de vidas como 7 através da variável "lives recebe 7".
 
@@ -35,16 +35,16 @@ def hangman(): # Cria a função do jogo.
     while len(word_letters) > 0 and lives > 0: # Enquanto as letras da palavra e as vidas forem maior que 0:
         print('\nVocê tem', lives, 'vidas restantes e já usou essas letras: ', ' '.join(used_letters)) # Imprime quantas vidas restam e junta pelo ".join", as letras já usadas.
 
-        word_list = [letter if letter in used_letters else '-' for letter in word] # Recebe a letra se estiver certa, senão um "-".
+        word_list = [letter if letter in used_letters else '-' for letter in word] # Armazena letras corretas ou "-" na variável "word_list".
         print(lives_visual[lives]) # Imprime o gráfico referente a quantidade de vidas restantes.
-        print('Palavra atual: ', ' '.join(word_list)) # Imprime as letras corretas ou "-". 
+        print('Palavra atual: ', ' '.join(word_list)) # Imprime as letras corretas já digitadas ou "-" para as que faltam.
 
-        user_letter = input('\nAdvinhe uma letra: ').upper() # Recebe e transforma letra digitada pelo usuário em maiúscula.
-        if user_letter in alphabet - used_letters:
-            used_letters.add(user_letter)
-            if user_letter in word_letters:
-                word_letters.remove(user_letter)
-                print('')
+        user_letter = input('\nAdvinhe uma letra: ').upper() # Recebe pelo "input" e transforma a letra digitada pelo usuário em maiúscula pelo ".upper()".
+        if user_letter in alphabet - used_letters: #......................................................
+            used_letters.add(user_letter) # .add() adiciona um elemento se ele não já estiver presente. No caso ao "used_letter" ou letras_usadas.
+            if user_letter in word_letters: # Se a letra recebida estiver nas letras da palavra a ser advinhada...
+                word_letters.remove(user_letter) # .remove() remove a letra digitada pelo usuário da palavra a ser advinhada.
+                print('') # Não imprime nada agora e volta para a imagem anterior de vidas restantes com a letra em "...já usou essas letras:...".
 
             else: # Senão:
                 lives = lives - 1  # Tira uma vida por errar.
@@ -57,7 +57,7 @@ def hangman(): # Cria a função do jogo.
             print('\nLetra ou caracter inválido.') # Imprime mensagem.
 
     # Chega aqui quando as letras que faltam advinhar ou as vidas forem 0.
-    if lives == 0: # Se vidas 0:
+    if lives == 0: # Se vidas igual 0:
         print(lives_visual[lives]) # Imprime imagem final da forca.
         print('Você morreu! A palavra era', word, '!\n') # Imprime mensagem e qual era a palavra.
         print('Leia mais sobre a linguagem em https://en.wikipedia.org/wiki/List_of_programming_languages \n') # Um link com mais informações sobre a linguagem.
